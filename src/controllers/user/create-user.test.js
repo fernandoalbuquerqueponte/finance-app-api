e4b84bdd8a8dce8aa840108241bd2eb8ef9dfc3c
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-10-02 17:30:15 -0300

    test: ensure 400 is returned if password is less than 6 chars

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -126,4 +126,25 @@ describe('Create User Controller', () => {
         // assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if password is less than 6 characters', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'Fernando',
+                last_name: 'Albuquerque',
+                email: 'example@example.com',
+                password: '123',
+            },
+        }
+
+        //act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
