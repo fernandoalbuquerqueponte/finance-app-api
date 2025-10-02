1d19fff736235f661f2a00e065a08bc6e5b388fb
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-10-02 16:22:39 -0300

    test: ensure 400 is returned if first_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -27,4 +27,24 @@ describe('Create User Controller', () => {
         expect(result.statusCode).toBe(201)
         expect(result.body).toBe(httpRequest.body)
     })
+
+    it('should return 400 if first_name is not provided', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                last_name: 'Albuquerque',
+                email: 'example@example.com',
+                password: '12345678910',
+            },
+        }
+        // act
+
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
