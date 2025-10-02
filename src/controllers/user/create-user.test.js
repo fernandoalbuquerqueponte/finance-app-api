b656c6ddd78cf5330af9a52cf1e2e64020877b60
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-10-02 16:50:30 -0300

    test: ensure 400 is returned if email is not provived

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -67,4 +67,24 @@ describe('Create User Controller', () => {
         // assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if email is not provided', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'Fernando',
+                last_name: 'Albuquerque',
+                password: '12345678910',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
