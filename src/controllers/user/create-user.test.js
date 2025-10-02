e5f0c40c9695c081abfa072d0a9ce244daa193b0
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-10-02 16:29:03 -0300

    test: ensure 400 is returned if last_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -47,4 +47,24 @@ describe('Create User Controller', () => {
         // assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if last_name is not provided', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'Fernando',
+                email: '',
+                password: '12345678910',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
