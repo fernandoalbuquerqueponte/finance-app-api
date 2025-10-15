871de0ea4b5ec80ca667424c6f093d663b10d806
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-10-15 14:52:31 -0300

    test: ensure UpdateUserController returns 400 if an invalid id is provided

--- src/controllers/user/update-user.test.js
+++ src/controllers/user/update-user.test.js
@@ -71,4 +71,20 @@ describe('UpdateUserController', () => {
         // assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 when an invalid id is provided', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const result = await sut.execute({
+            params: {
+                userId: 'invalid-id',
+            },
+            body: httpRequest.body,
+        })
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
