ac38af386f8ef23ef07cbbb9080ec300eb10e0e2
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-10-23 16:24:36 -0300

    test: ensure UpdateTransactionController returns 400 when type is invalid

--- src/controllers/transaction/update-transaction.test.js
+++ src/controllers/transaction/update-transaction.test.js
@@ -93,4 +93,21 @@ describe('Update Transaction Controller', () => {
         // assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 when type is invalid', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const result = await sut.execute({
+            ...baseHttpRequest,
+            body: {
+                ...baseHttpRequest.body,
+                type: 'invalid_type',
+            },
+        })
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
