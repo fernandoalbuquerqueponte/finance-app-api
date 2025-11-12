9fb469b2100be9a8a0d65751b503a9ac1500c869
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-11-12 17:43:31 -0300

    test: ensure CreateUserUseCase throws if IdGeneratorAdapter throws

--- src/use-cases/user/create-user.test.js
+++ src/use-cases/user/create-user.test.js
@@ -137,4 +137,18 @@ describe('Create User Use Case', () => {
         // assert
         expect(promise).rejects.toThrow()
     })
+
+    it('should throw if IdGeneratorAdapter throws', () => {
+        // arrange
+        const { sut, idGeneratorAdapter } = makeSut()
+        jest.spyOn(idGeneratorAdapter, 'execute').mockImplementationOnce(() => {
+            throw new Error()
+        })
+
+        // act
+        const promise = sut.execute(user)
+
+        // assert
+        expect(promise).rejects.toThrow()
+    })
 })
