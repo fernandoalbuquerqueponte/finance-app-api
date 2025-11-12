4e58d7a2bb60bdc981368768ed94a0344ef9b0e8
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-11-12 17:04:56 -0300

    test: ensure CreateUserUseCase calls PasswordHasherAdapter to cryptograph password

--- src/use-cases/user/create-user.test.js
+++ src/use-cases/user/create-user.test.js
@@ -102,4 +102,25 @@ describe('Create User Use Case', () => {
             id: 'generated_id',
         })
     })
+
+    it('should call PasswordHasherAdapter to cryptograph password', async () => {
+        // arrange
+        const { sut, createUserRepository, passwordHasherAdapter } = makeSut()
+        const passwordHasherSpy = jest.spyOn(passwordHasherAdapter, 'execute')
+        const createUserRepositorySpy = jest.spyOn(
+            createUserRepository,
+            'execute',
+        )
+
+        // act
+        await sut.execute(user)
+
+        // assert
+        expect(passwordHasherSpy).toHaveBeenCalledWith(user.password)
+        expect(createUserRepositorySpy).toHaveBeenCalledWith({
+            ...user,
+            password: 'hashed_password',
+            id: 'generated_id',
+        })
+    })
 })
