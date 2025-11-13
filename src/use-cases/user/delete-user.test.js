cddc7cff7e9b4a18d2c654b5e07df35d6472130b
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-11-13 17:01:06 -0300

    test: ensure DeleteUserUseCase throws if DeleteUserRepository throws

--- src/use-cases/user/delete-user.test.js
+++ src/use-cases/user/delete-user.test.js
@@ -46,4 +46,18 @@ describe('DeleteUserUseCase', () => {
         // assert
         expect(executeSpy).toHaveBeenCalledWith(userId)
     })
+
+    it('should throw if DeleteUserRepository throws', async () => {
+        // arrange
+        const { sut, deleteUserRepository } = makeSut()
+        jest.spyOn(deleteUserRepository, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        // act
+        const promise = sut.execute(faker.string.uuid())
+
+        // assert
+        await expect(promise).rejects.toThrow()
+    })
 })
