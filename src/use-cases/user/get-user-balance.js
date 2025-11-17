9f0ab9a1c8d8ab8a76d4d967635e2f575eca1944
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-11-17 10:37:43 -0300

    fix: ensure GetUserBalanceUseCase passes userId to UserNotFoundError

--- src/use-cases/user/get-user-balance.js
+++ src/use-cases/user/get-user-balance.js
@@ -9,7 +9,7 @@ export class GetUserBalaceUseCase {
         const user = await this.getUserByIdRepository.execute(userId)
 
         if (!user) {
-            throw new UserNotFoundError()
+            throw new UserNotFoundError(userId)
         }
 
         const balance = await this.getUserBalanceRepository.execute(userId)
