327009292db82d2f258b270bab1f9c76be9dc233
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-11-26 15:48:27 -0300

    test: ensure GetTransactionsByUserIdUseCase throws if GetTransactionsByUserIdRepo throws

--- src/use-cases/transaction/get-transactions-by-user-id.test.js
+++ src/use-cases/transaction/get-transactions-by-user-id.test.js
@@ -110,4 +110,20 @@ describe('GetTransactionsByUserIdUseCase', () => {
         // assert
         await expect(promise).rejects.toThrow()
     })
+
+    it('should throw if GetTransactionsByUserIdRepository throws', async () => {
+        // arrange
+        const { sut, getTransactionsByUserIdRepository } = makeSut()
+        jest.spyOn(
+            getTransactionsByUserIdRepository,
+            'execute',
+        ).mockRejectedValueOnce(new Error())
+        const userId = faker.string.uuid()
+
+        // act
+        const promise = sut.execute(userId)
+
+        // assert
+        await expect(promise).rejects.toThrow()
+    })
 })
