326ff7d726fcfa0009ece28457bc7c80a2b74a17
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-11-27 19:50:28 -0300

    test: ensure CreateTransactionUseCase and CreateUserUse case returns fixtures on stubs

--- src/use-cases/transaction/create-transaction.test.js
+++ src/use-cases/transaction/create-transaction.test.js
@@ -9,7 +9,7 @@ describe('CreateTransactionUseCase', () => {
     }
 
     class CreateTransactionRepositoryStub {
-        async execute(transaction) {
+        async execute() {
             return transaction
         }
     }
@@ -53,10 +53,7 @@ describe('CreateTransactionUseCase', () => {
         const result = await sut.execute(createTransactionParams)
 
         // assert
-        expect(result).toEqual({
-            ...createTransactionParams,
-            id: 'random_id',
-        })
+        expect(result).toEqual(transaction)
     })
 
     it('should call GetUserByEmailRepository with correct params', async () => {
