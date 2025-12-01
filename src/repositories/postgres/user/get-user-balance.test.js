45f20b519b42bef838e8221bfad692c42da7ad63
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-12-01 09:43:46 -0300

    test: ensure PostgresGetUserBalanceRepository gets user balance

---
+++ src/repositories/postgres/user/get-user-balance.test.js
@@ -0,0 +1,66 @@
+import { PostgresGetUserBalanceRepository } from './get-user-balance.js'
+import { prisma } from '../../../../prisma/prisma.js'
+import { user as fakeUser } from '../../../tests/index.js'
+import { faker } from '@faker-js/faker'
+
+describe('PostgresGetUserBalanceRepository', () => {
+    it('should get user balance on db', async () => {
+        const user = await prisma.user.create({ data: fakeUser })
+
+        await prisma.transaction.createMany({
+            data: [
+                {
+                    name: faker.string.sample(),
+                    amount: 5000,
+                    type: 'EARNING',
+                    date: faker.date.recent(),
+                    user_id: user.id,
+                },
+                {
+                    name: faker.string.sample(),
+                    amount: 5000,
+                    type: 'EARNING',
+                    date: faker.date.recent(),
+                    user_id: user.id,
+                },
+                {
+                    name: faker.string.sample(),
+                    amount: 1000,
+                    type: 'EXPENSE',
+                    date: faker.date.recent(),
+                    user_id: user.id,
+                },
+                {
+                    name: faker.string.sample(),
+                    amount: 1000,
+                    type: 'EXPENSE',
+                    date: faker.date.recent(),
+                    user_id: user.id,
+                },
+                {
+                    name: faker.string.sample(),
+                    amount: 4000,
+                    type: 'INVESTMENT',
+                    date: faker.date.recent(),
+                    user_id: user.id,
+                },
+                {
+                    name: faker.string.sample(),
+                    amount: 4000,
+                    type: 'INVESTMENT',
+                    date: faker.date.recent(),
+                    user_id: user.id,
+                },
+            ],
+        })
+
+        const sut = new PostgresGetUserBalanceRepository()
+
+        const result = await sut.execute(user.id)
+
+        expect(result.earnings.toString()).toBe('10000')
+        expect(result.expenses.toString()).toBe('2000')
+        expect(result.investments.toString()).toBe('8000')
+        expect(result.balance.toString()).toBe('0')
+    })
+})
