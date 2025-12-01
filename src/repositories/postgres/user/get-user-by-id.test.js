27143addf9380d79e415ba1a3e3a35ba33669135
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-12-01 10:55:08 -0300

    test: ensure PostgresGetUserByIdRepository calls prisma with correct params

--- src/repositories/postgres/user/get-user-by-id.test.js
+++ src/repositories/postgres/user/get-user-by-id.test.js
@@ -11,4 +11,17 @@ describe('PostgresGetUserByIdRepository', () => {
 
         expect(result).toStrictEqual(user)
     })
+
+    it('should call prisma with correct params', async () => {
+        const sut = new PostgresGetUserByIdRepository()
+        const prismaSpy = jest.spyOn(prisma.user, 'findUnique')
+
+        await sut.execute(fakeUser.id)
+
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                id: fakeUser.id,
+            },
+        })
+    })
 })
