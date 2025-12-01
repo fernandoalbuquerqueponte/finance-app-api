126f06f362735e9fc4b0bae57e7dc85f02c7c7b2
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-12-01 09:43:04 -0300

    fix: ensure transactions are deleted first on jest after env setup file

--- jest.setup-after-env.js
+++ jest.setup-after-env.js
@@ -1,7 +1,6 @@
 import { prisma } from './prisma/prisma.js'
 
 beforeEach(async () => {
-    console.log('ferbabdo de kiubdiiii')
-    await prisma.user.deleteMany({})
     await prisma.transaction.deleteMany({})
+    await prisma.user.deleteMany({})
 })
