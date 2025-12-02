3e2bf6d9c9f2df30a99f02dc3d50bc1c7959c8c1
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-12-02 17:32:23 -0300

    test: ensure makeUpdateTransactionController returns a valid UpdateTransactionController

--- src/factories/controllers/transaction.test.js
+++ src/factories/controllers/transaction.test.js
@@ -1,10 +1,22 @@
-import { makeCreateTransactionController } from './transaction.js'
-import { CreateTransactionController } from '../../controllers/index.js'
+import {
+    makeCreateTransactionController,
+    makeUpdateTransactionController,
+} from './transaction.js'
+import {
+    CreateTransactionController,
+    UpdateTransactionController,
+} from '../../controllers/index.js'
 
 describe('Transaction Controller Factories', () => {
     it('should return a valid CreateTransactionController instance', () => {
         expect(makeCreateTransactionController()).toBeInstanceOf(
             CreateTransactionController,
+        )
+    })
+
+    it('should return a valid UpdateTransactionController instance', () => {
+        expect(makeUpdateTransactionController()).toBeInstanceOf(
+            UpdateTransactionController,
         )
     })
 })
