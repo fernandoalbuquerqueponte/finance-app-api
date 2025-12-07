9eb8b4eb9813cf5f187542252df43a4d55b61858
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-12-07 15:05:46 -0300

    feat: ensure UpdateTransactionController returns 404 when TransactionNotFoundError is thrown

--- src/controllers/transaction/update-transaction.js
+++ src/controllers/transaction/update-transaction.js
@@ -6,7 +6,9 @@ import {
     invalidIdResponse,
     ok,
     serverError,
+    transactionNotFoundResponse,
 } from '../helpers/index.js'
+import { TransactionNotFoundError } from '../../errors/transaction.js'
 
 export class UpdateTransactionController {
     constructor(updateTransactionUseCase) {
@@ -36,6 +38,10 @@ export class UpdateTransactionController {
                 return badRequest({
                     message: error.errors[0].message,
                 })
+            }
+
+            if (error instanceof TransactionNotFoundError) {
+                return transactionNotFoundResponse()
             }
 
             console.error(error)
