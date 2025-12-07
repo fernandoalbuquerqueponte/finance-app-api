182205afcb151f5e0657344282cb4db336a20e6b
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-12-07 15:00:57 -0300

    feat: ensure UpdateTransactionRepository throws  TransactionNotFoundError if transaction is not found

--- src/repositories/postgres/transaction/update-transaction.js
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -1,12 +1,26 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 import { prisma } from '../../../../prisma/prisma.js'
+import { TransactionNotFoundError } from '../../../errors/transaction.js'
 
 export class PostgresUpdateTransactionRepository {
     async execute(transactionId, updateTransactionParams) {
-        return await prisma.transaction.update({
-            where: {
-                id: transactionId,
-            },
-            data: updateTransactionParams,
-        })
+        try {
+            return await prisma.transaction.update({
+                where: {
+                    id: transactionId,
+                },
+                data: updateTransactionParams,
+            })
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                const code = error.code
+
+                // p2025 = "An operation failed because it depends on one or more records that were required but not found. {cause}"
+                if (code === 'P2025') {
+                    throw new TransactionNotFoundError(transactionId)
+                }
+            }
+            throw error
+        }
     }
 }
