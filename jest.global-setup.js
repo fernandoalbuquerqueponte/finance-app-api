4190e5d8af5fa469f2a9e9c0af7ff2b6e888c020
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-12-15 04:21:00 -0300

    ci: fix test run on gh actions

--- jest.global-setup.js
+++ jest.global-setup.js
@@ -1,6 +1,6 @@
 const { execSync } = require('child_process')
 
 module.exports = async () => {
-    execSync('docker-compose up -d --wait', { stdio: 'inherit' })
+    execSync('docker compose up -d --wait', { stdio: 'inherit' })
     execSync('npx prisma db push', { stdio: 'inherit' })
 }
--- src/routes/transactions.e2e.test.js
+++ src/routes/transactions.e2e.test.js
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import { app } from '../app'
+import { app } from '../app.cjs'
 import { transaction, user } from '../tests'
 import { TransactionType } from '@prisma/client'
 
